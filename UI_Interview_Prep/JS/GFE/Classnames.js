f050b79b5c0f5dc8f29b4e1650447407f5e2c299
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-04-18 13:25:44 +0530

    classNames ||

--- UI_Interview_Prep/JS/GFE/Classnames.js
+++ UI_Interview_Prep/JS/GFE/Classnames.js
@@ -1,20 +1,23 @@
 function classNames(...args) {
-  let str = [];
+  let str = new Set();
   for (let ar of args) {
     if (!ar) continue;
     else if (typeof ar === "string" || typeof ar === "number") {
-      str.push(ar);
+      str.add(ar);
     } else if (Array.isArray(ar)) {
-      str.push(classNames(...ar));
+      str.add(classNames(...ar));
     } else if (typeof ar === "object" && ar !== null) {
       for (let key in ar) {
         if (ar[key]) {
-          str.push(key);
+          str.add(key);
         }
       }
+    } else if (typeof ar === "function") {
+      let result = ar();
+      str.add(classNames(result));
     }
   }
-  return str.join(" ");
+  return [...str].join(" ");
 }
 
 classNames("foo", "bar"); // 'foo bar'
@@ -25,4 +28,5 @@ classNames({ foo: true }, { bar: true }); // 'foo bar'
 classNames({ foo: true, bar: true }); // 'foo bar'
 classNames({ foo: true, bar: false, qux: true }); // 'foo qux'
 // console.log(classNames(["foo", "bar", "baz"]));
-console.log(classNames(null, false, "bar", undefined, 0, 1, { baz: null }, ""));
+// console.log(classNames(null, false, "bar", undefined, 0, 1, { baz: null }, ""));
+console.log(classNames(() => "foo", "bar"));
