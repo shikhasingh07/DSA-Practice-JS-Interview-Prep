224cf7b9a5b12551c09343b9ab1bb52321b8a12f
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-04-19 23:35:11 +0530

    backspaceCompare

--- ArrayHashMap/TwoPointer/844.js
+++ ArrayHashMap/TwoPointer/844.js
@@ -1,42 +1,19 @@
-var backspaceCompare = function(s, t) {
-    let i = s.length-1; 
-    let j = t.length-1;
+function compre(str) {
+  let stack = [];
 
-    let skipi = 0 ; 
-    let skipj = 0 ; 
-    while(i >= 0 || j >= 0){
-        while( i >= 0){
-            if(s[i] === "#"){
-                skipi++; 
-                i--;
-            }else if(skipi > 0){
-                skipi--; 
-                i--;
-            }else{
-                break;
-            }
-        }
-        while(j >= 0 ){
-            if(t[j] === "#"){
-                skipj++; 
-                j--;
-            }else if(skipj > 0){
-                skipj--; 
-                j--; 
-            }else {
-                break; 
-            }
-        }
-        if( i >= 0 && j >= 0 && s[i] !== t[j]) {
-            return false;
-        }
-        if ((i >= 0) !== (j >= 0)) {
-            return false;
-        }
-        i--;
-        j--; 
+  for (let ch of str) {
+    if (ch === "#") {
+      stack.pop();
+    } else {
+      stack.push(ch);
     }
-    return true;
+  }
+
+  return stack.join("");
+}
+
+var backspaceCompare = function (s, t) {
+  return compre(s) === compre(t);
 };
 
-console.log(backspaceCompare("ab##","cd##"))
+console.log(backspaceCompare("ab#", "cd##"));
