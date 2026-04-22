71a99e552a6b79c241886d12108f5de09462bb7d
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-04-22 23:22:02 +0530

    quest:3Sum Closest

--- ArrayHashMap/TwoPointer/sums/16.js
+++ ArrayHashMap/TwoPointer/sums/16.js
@@ -1,5 +1,27 @@
-var threeSumClosest = function(nums, target) {
-    
+var threeSumClosest = function (nums, target) {
+  nums.sort((a, b) => a - b);
+
+  let closed = nums[0] + nums[1] + nums[2];
+  for (let index = 0; index < nums.length; index++) {
+    if (nums[index] && nums[index] === nums[index - 1]) continue;
+
+    let left = index + 1,
+      right = nums.length - 1;
+    while (left < right) {
+      let sum = nums[index] + nums[left] + nums[right];
+      if (Math.abs(sum - target) < Math.abs(closed - target)) {
+        closed = sum;
+      }
+      if(sum < target) {
+        left++;
+      }else if(sum > target){
+        right --; 
+      }else {
+        return target; 
+      }
+    }
+  }
+  return closed;
 };
 
-console.log(threeSumClosest())
+console.log(threeSumClosest());
