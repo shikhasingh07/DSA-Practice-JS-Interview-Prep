fbe005af5b2cf146f38fa71296a010746cf056cf
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-05-10 19:46:44 +0530

    quest-ui:curry

--- UI_Interview_Prep/JS/Easy/curry.js
+++ UI_Interview_Prep/JS/Easy/curry.js
@@ -1,6 +1,17 @@
 function curry (fn) { 
     return function ar(...arg){
-        if(arg.length >= fn.length) return fn(...arg); 
-        return (...arg2) => ar(...arg , ...arg2)
+        if(arg.length >= fn.length) return fn.call(this,...arg); 
+        return (...arg2) => ar.call(this,...arg , ...arg2);
     }
 }
+function add(a, b) {
+  return a + b;
+}
+
+const curriedAdd = curry(add);
+curriedAdd(3)(4); // 7
+
+const alreadyAddedThree = curriedAdd(3);
+alreadyAddedThree(4); // 7
+
+
