ff5b323443a642f2802903f180c4ed7c24cd24b8
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-05-21 22:46:13 +0530

    quest:1717. Maximum Score From Removing Substrings

--- Greedy/medium/String/1717.js
+++ Greedy/medium/String/1717.js
@@ -1,8 +1,26 @@
-var maximumGain = function(s, x, y) {
-    
-    let stack = [];
-    
+function remove(s, pair, pts) {
+  let stack = [],
+    score = 0;
+  for (let char of s) {
+    if (stack.length && stack[stack.length - 1] + char === pair) {
+      stack.pop();
+      score += pts;
+    } else {
+      stack.push(char);
+    }
+  }
+  return { score, remaining: stack.join("") };
+}
+
+var maximumGain = function (s, x, y) {
+  let first = y > x ? "ba" : "ab";
+  let second = y > x ? "ab" : "ba";
+  let r1 = remove(s, first, Math.max(x, y));
+  let r2 = remove(r1.remaining, second, Math.min(x, y));
+  return r1.score + r2.score;
 };
 
-let s = "cdbcbbaaabab", x = 4, y = 5;
-console.log(maximumGain(s,x,y))
+let s = "cdbcbbaaabab",
+  x = 4,
+  y = 5;
+console.log(maximumGain(s, x, y));
