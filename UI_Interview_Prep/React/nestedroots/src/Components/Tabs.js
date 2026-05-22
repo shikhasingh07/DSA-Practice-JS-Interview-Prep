3a05a8ad91c0fcdb0e6bc1c68e831f9b30bee6f0
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-05-22 23:21:14 +0530

    quest:ui

--- UI_Interview_Prep/React/nestedroots/src/Components/Tabs.js
+++ UI_Interview_Prep/React/nestedroots/src/Components/Tabs.js
@@ -5,12 +5,39 @@ export default function Tabs() {
 
   return (
     <div>
-      <div>
-        <button style={{color: activeTab === 1 ? 'blue' : 'black'}} onClick={() => setActiveTab(1)}>HTML</button>
-        <button style={{color: activeTab === 2 ? 'blue' : 'black'}} onClick={() => setActiveTab(2)}>CSS</button>
-        <button style={{color: activeTab === 3 ? 'blue' : 'black'}} onClick={() => setActiveTab(3)}>JavaScript</button>
+      <div role="tablist">
+        <button
+          id="tab-1"
+          role="tab"
+          aria-controls="panel-1"
+          aria-selected={activeTab === 1 ? true : false}
+          style={{ color: activeTab === 1 ? "blue" : "black" }}
+          onClick={() => setActiveTab(1)}
+        >
+          HTML
+        </button>
+        <button
+          id="tab-2"
+          role="tab"
+          aria-controls="panel-2"
+          aria-selected={activeTab === 2 ? true : false}
+          style={{ color: activeTab === 2 ? "blue" : "black" }}
+          onClick={() => setActiveTab(2)}
+        >
+          CSS
+        </button>
+        <button
+          id="tab-3"
+          role="tab"
+          aria-controls="panel-3"
+          aria-selected={activeTab === 3 ? true : false}
+          style={{ color: activeTab === 3 ? "blue" : "black" }}
+          onClick={() => setActiveTab(3)}
+        >
+          JavaScript
+        </button>
       </div>
-      <div>
+      <div  role="tabpanel" id={`panel-${activeTab}`} aria-labelledby={`tab-${activeTab}`}>
         {activeTab === 1 && (
           <p>
             The HyperText Markup Language or HTML is the standard markup
