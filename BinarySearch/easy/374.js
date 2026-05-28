1f53e8a4c913799bf2b9da386fc71ace3a84114b
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-05-28 22:46:54 +0530

    374. Guess Number Higher or Lower

---
+++ BinarySearch/easy/374.js
@@ -0,0 +1,20 @@
+var guessNumber = function (n) {
+  let low = 1,
+    high = n;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    let res = guess(mid);
+
+    if (res === 0) {
+      return mid;
+    } else if (res === -1) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return -1;
+};
+
+console.log(guessNumber());
