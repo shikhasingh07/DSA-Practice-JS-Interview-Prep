c4a1fd663c90df97a4c40b8d62f3a49ec14d0447
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-05-29 01:32:48 +0530

    69. Sqrt(x)

--- BinarySearch/easy/69.js
+++ BinarySearch/easy/69.js
@@ -1,8 +1,20 @@
-var mySqrt = function(x) {
-    
-    if(x <= 2) return x;
+var mySqrt = function (x) {
+  if (x <= 2) return 1;
 
-    
+  let low = 1,
+    high = x;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    let hmid = mid * mid;
+    if (hmid === x) {
+      return mid;
+    } else if (hmid < x) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return high;
 };
 let x = 8;
-console.log(mySqrt(x))
+console.log(mySqrt(x));
