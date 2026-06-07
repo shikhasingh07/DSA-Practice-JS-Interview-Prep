571a7def6975aa34c1c070907474f184c9244a16
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-06-07 18:16:33 +0530

    Integer to Roman

---
+++ String/medium/12.js
@@ -0,0 +1,28 @@
+var intToRoman = function (num) {
+  const symbol = [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ];
+
+  let value = "";
+  for (let [val, sym] of symbol) {
+    while (num >= val) {
+      value += sym;
+      num -= val;
+    }
+  }
+  return value;
+};
+let num = 3749;
+console.log(intToRoman(num));
