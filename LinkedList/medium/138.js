c719a9151a7cde2f7fbe827eef7bf82f18741f6b
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-06-10 21:30:07 +0530

    Copy List with Random Pointer

--- LinkedList/medium/138.js
+++ LinkedList/medium/138.js
@@ -1,5 +1,25 @@
-var copyRandomList = function(head) {
-    
+var copyRandomList = function (head) {
+  let map = new Map();
+  let curr = head;
+  while (curr !== null) {
+    map.set(curr, new Node(curr.val));
+    curr = curr.next;
+  }
+
+  curr = head;
+  while (curr !== null) {
+    map.get(curr).next = map.get(curr.next) ?? null;
+    map.get(curr).random =  map.get(curr.random) ??  null;
+    curr = curr.next;
+  }
+
+  return map.get(head);
 };
-let head = [[7,null],[13,0],[11,4],[10,2],[1,0]];
+let head = [
+  [7, null],
+  [13, 0],
+  [11, 4],
+  [10, 2],
+  [1, 0],
+];
 console.log(copyRandomList(head));
