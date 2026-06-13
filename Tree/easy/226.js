b79b3f1ce1eca3ea8cee7e6e2de74b0821f79ab0
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-06-14 01:49:23 +0530

    226. Invert Binary Tree

--- Tree/easy/226.js
+++ Tree/easy/226.js
@@ -1,3 +1,11 @@
 var invertTree = function(root) {
-    
+    if(root === null) return 
+
+    [root.left , root.right] = [root.right , root.left];
+    invertTree(root.left); 
+    invertTree(root.right); 
+
+    return root;
 };
+let root = [4,2,7,1,3,6,9];
+console.log(invertTree(root))
