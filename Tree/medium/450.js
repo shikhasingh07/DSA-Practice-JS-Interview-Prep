7958240f877df63c9b0d23f0d4a0483c45efe1f9
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-06-15 21:45:55 +0530

    deleteNode

--- Tree/medium/450.js
+++ Tree/medium/450.js
@@ -1,5 +1,22 @@
 var deleteNode = function(root, key) {
-    
+    if (root === null) return null;
+
+    if (root.val > key) {
+        root.left = deleteNode(root.left, key);
+    }
+    else if (root.val < key) {
+        root.right = deleteNode(root.right, key);
+    }
+    else {
+        if (!root.left) return root.right;
+        if (!root.right) return root.left;
+        let minNode = root.right;
+        while (minNode.left) minNode = minNode.left;
+
+        root.val = minNode.val;
+        root.right = deleteNode(root.right, minNode.val);
+    }
+    return root;
 };
 let root = [5,3,6,2,4,null,7], key = 3;
 console.log(deleteNode(root,key))
