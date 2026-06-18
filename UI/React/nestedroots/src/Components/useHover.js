a641694dd170104c60b2e5892cd9265565d3983e
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-06-18 19:47:19 +0530

    UI-useHover

---
+++ UI/React/nestedroots/src/Components/useHover.js
@@ -0,0 +1,27 @@
+import { useEffect, useState, useRef } from "react";
+const useHover = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const ref = useRef(null);
+
+  const refCallback = (element) => {
+    ref.current = element;
+  };
+
+  useEffect(() => {
+    const element = ref.current;
+    if (!element) return;
+
+    const handleMouseEnter = () => setIsHovered(true);
+    const handleMouseLeave = () => setIsHovered(false);
+
+    element.addEventListener("mouseenter", handleMouseEnter);
+    element.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      element.removeEventListener("mouseenter", handleMouseEnter);
+      element.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
+
+  return [refCallback, isHovered]; // kya return karogi?
+};
