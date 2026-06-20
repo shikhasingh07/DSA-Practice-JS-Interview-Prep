68ca22bcfa5a6756ed261895a6e9357a30217da6
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-06-20 16:31:12 +0530

    trie:208. Implement Trie (Prefix Tree)

---
+++ Trie/CreateTrie.js
@@ -0,0 +1,56 @@
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.isEnd = false;
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word){
+    let node = this.root; 
+
+    for(let char of word){
+        if(!node.children[char]){
+            node.children[char] = new TrieNode();
+        }
+        node = node.children[char];
+    }
+
+    node.isEnd = true;
+  }
+
+  search(word) {
+    let node = this.root 
+    for(let char of word){
+        if(!node.children[char]){
+            return false;
+        }
+        node = node.children[char];
+    }
+    return node.isEnd;
+  }
+
+  startsWith(prefix){
+     let node = this.root 
+    for(let char of prefix){
+        if(!node.children[char]){
+            return false;
+        }
+        node = node.children[char];
+    }
+    return true;
+  }
+}
+
+const trie = new Trie();
+trie.insert("apple");
+console.log(trie.search("apple"));    // true
+console.log(trie.search("app"));      // false
+console.log(trie.startsWith("app"));  // true
+trie.insert("app");
+console.log(trie.search("app"));      // true
+
