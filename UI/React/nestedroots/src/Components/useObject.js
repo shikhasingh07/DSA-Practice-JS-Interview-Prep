59190eb4092dc1768702f957f96deeb98db1f791
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-06-24 10:27:04 +0530

    UI

--- UI/React/nestedroots/src/Components/useObject.js
+++ UI/React/nestedroots/src/Components/useObject.js
@@ -3,16 +3,16 @@ import React, { useState } from "react";
 const useObject = (initialValue) => {
   const [state, setState] = useState(initialValue);
 
-  const isPlainObject = (val) => 
-  val !== null && typeof val === 'object' && !Array.isArray(val);
+  const isPlainObject = (val) =>
+    val !== null && typeof val === "object" && !Array.isArray(val);
 
-const setObject = (updater) => {
-  setState((prev) => {
-    const newVal = typeof updater === 'function' ? updater(prev) : updater;
-    if (!isPlainObject(newVal)) throw new Error('Invalid new state');
-    return { ...prev, ...newVal };
-  });
-};
+  const setObject = (updater) => {
+    setState((prev) => {
+      const newVal = typeof updater === "function" ? updater(prev) : updater;
+      if (!isPlainObject(newVal)) throw new Error("Invalid new state");
+      return { ...prev, ...newVal };
+    });
+  };
 
   return [state, setObject];
 };
