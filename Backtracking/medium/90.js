43b43e0a3d88c9d1853c1d306a4a678f825df512
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-06-28 23:19:43 +0530

    subsetsWithDup

--- Backtracking/medium/90.js
+++ Backtracking/medium/90.js
@@ -1,5 +1,18 @@
-var subsetsWithDup = function(nums) {
-    
+function result(nums, n, idx, current, ans) {
+    ans.push([...current]);
+
+    for (let i = idx; i < n; i++) {
+        if (i > idx && nums[i] === nums[i - 1]) continue;
+
+        current.push(nums[i]);
+        result(nums, n, i + 1, current, ans);
+        current.pop();
+    }
+}
+var subsetsWithDup = function (nums) {
+    let ans = [];
+    result(nums.sort((a, b) => a - b), nums.length, 0, [], ans);
+    return ans;
 };
-let nums = [1,2,2];
-console.log()
+let nums = [1, 2, 2];
+console.log(subsetsWithDup(nums))
