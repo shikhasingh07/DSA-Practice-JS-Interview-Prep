3637e86796d311bd9389ad3d632ee08e54327adf
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-07-12 15:05:17 +0530

    numSquares

---
+++ DP/UnboundKnapSack/279.js
@@ -0,0 +1,15 @@
+var numSquares = function (n) {
+    const dp = new Array(n + 1).fill(Infinity);
+
+    dp[0] = 0;
+    for (let i = 0; i <= n; i++) {
+        for (let j = 0; j * j <= i; j++) {
+            dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+        }
+    }
+   
+   
+    return dp[n];
+};
+let n = 4///;
+console.log(numSquares(n));
