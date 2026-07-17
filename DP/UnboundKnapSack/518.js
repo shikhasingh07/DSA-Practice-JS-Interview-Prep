86eef4e87f529f6ed17d64cc817bb2dfa07fdd58
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-07-17 23:55:36 +0530

    518. Coin Change II

---
+++ DP/UnboundKnapSack/518.js
@@ -0,0 +1,14 @@
+var change = function (amount, coins) {
+    let dp = new Array(amount + 1).fill(0);
+
+    dp[0] = 1;
+    for (let coin of coins) {
+        for (let j = coin; j <= amount; j++) {
+           dp[j] += dp[j - coin];
+        }   
+    }
+
+   return dp[amount];
+};
+let amount = 5, coins = [1, 2, 5];
+console.log(change(amount, coins))
