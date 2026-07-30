a54b8ba9a61cb6d70a05044e799d47060df0eec3
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-07-31 00:02:07 +0530

    312. Burst Balloons

--- DP/OtherDP/Hard/312.js
+++ DP/OtherDP/Hard/312.js
@@ -1,6 +1,24 @@
-var maxCoins = function(nums) {
-    
+var maxCoins = function (nums) {
+    nums = [1, ...nums, 1];
+    let n = nums.length;
+    let dp = new Array(n).fill(0).map(() => new Array(n).fill(0));
+
+
+    for (let len = 2; len < n; len++) {
+        for (let left = 0; left < n - len; left++) {
+            let right = left + len;
+            for (let k = left + 1; k < right; k++) {
+                dp[left][right] = Math.max(
+                    dp[left][right],
+                    nums[left] * nums[k] * nums[right] + dp[left][k] + dp[k][right]
+                );
+            }
+
+        }
+    }
+
+    return dp[0][n - 1];
 };
 
-let nums = [3,1,5,8];
+let nums = [3, 1, 5, 8];
 console.log(maxCoins(nums));
