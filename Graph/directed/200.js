a1769e64549934b53554d9babd86d1ec6b3db097
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-07-31 23:40:47 +0530

    numIslands

---
+++ Graph/directed/200.js
@@ -0,0 +1,11 @@
+var numIslands = function(grid) {
+    let visited = new Set();
+    let count = 0; 
+};
+let grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]];
+
+console.log(numIslands(grid))
