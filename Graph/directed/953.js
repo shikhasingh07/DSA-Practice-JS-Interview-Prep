17c90e3a7902375f637d7066e8ae17df367c3b7c
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-08-03 01:13:34 +0530

    953. Verifying an Alien Dictionary

---
+++ Graph/directed/953.js
@@ -0,0 +1,21 @@
+var isAlienSorted = function (words, order) {
+    let map = {};
+
+    for (let i = 0; i < order.length; i++) {
+        map[order[i]] = i;
+    }
+
+    for (let i = 0; i < words.length - 1; i++) {
+        let w1 = words[i], w2 = words[i + 1];
+        for (let j = 0; j < w1.length; j++) {
+            if (j >= w2.length) return false;
+            if (map[w1[j]] < map[w2[j]]) break;
+            if (map[w1[j]] > map[w2[j]]) return false;
+        }
+    }
+
+    return true;
+};
+
+let words = ["word", "world", "row"], order = "worldabcefghijkmnpqstuvxyz"
+console.log(isAlienSorted(words, order))
